f4d1759e64ff8b44fdd2e1f742ea0018333720c8
filip9322 <filip9322@gmail.com>
2018-06-29 01:35:59 -0500

    Se inicia proyecto, ya se seteo migration y credenciales para bd

---
+++ database/migrations/2018_06_29_061308_create_TODO_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTODOTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('TODO', function (Blueprint $table) {
+            $table->increments('rowid');
+            $table->string('name_TODO');
+            $table->index('created_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('TODO');
+    }
+}
